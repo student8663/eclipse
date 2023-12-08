4e15651026adbab12fc1ff7a1436d008ddceba0a
student8663 <110198971+student8663@users.noreply.github.com>
2023-12-08 14:52:32 -0800

    Add files via upload

--- InputHandler.java
+++ InputHandler.java
@@ -1,42 +1,52 @@
-package Dummy;
+package CP;
+import java.time.*;
 import java.util.*;
-// 처음꺼는 enter key 눌르면 오류가 안뜨고 기다림 하지만 두번째거는 immediately return false 
-// scanner.hasNextInt(), it doesn't immediately return false because the hasNextInt() method is looking for the next token (in this case, an integer). 
-// It doesn't consume the newline character until you attempt to read the actual integer using a method like nextInt().
+
 public class InputHandler {
-	private static Scanner input = new Scanner(System.in);
+	public static Scanner scanner = new Scanner(System.in);
+	public static LocalDate date;
 	
-	public static int getInt(String string) {
+	public static LocalDate getValidDate(String string) {
 		System.out.println(string);
-		while(!input.hasNextInt()) {
-			System.out.println("Wrong input");
-			input.nextLine();
-		}
-		
-		int userInput = input.nextInt();
-		input.nextLine();
-		
-		return userInput;
-	}
-	
-	public static int getInt2(String string) {
-		String userStringInput;
-		int userIntInput;
-		//while print store try parse return catch print 
-		while(true) {
-			System.out.println(string);
-			userStringInput = input.nextLine();
-			try {
-				userIntInput = Integer.parseInt(userStringInput);
-				return userIntInput;
-			}catch(NumberFormatException ex) {
-				System.out.println("invalid input");
+		int month, day, year;
+		do {
+			System.out.print("Month: ");
+			while(!scanner.hasNextInt()) {
+				System.out.print("invalid input try again ");
+				scanner.nextLine();
 			}
-		}
+			month = scanner.nextInt();
+			
+			System.out.print("Day: ");
+			while(!scanner.hasNextInt()) {
+				System.out.print("invalid input try again ");
+				scanner.nextLine();
+			}
+			day = scanner.nextInt();
+			
+			System.out.print("Year: ");
+			while(!scanner.hasNextInt()) {
+				System.out.print("invalid input try again ");
+				scanner.nextLine();
+			}
+			year = scanner.nextInt();
+			
+			
+		}while(!isValidDate(month, day, year));
+		
+		return date;
+		
 	}
 	
-	
-	
+	public static boolean isValidDate(int month, int day, int year) {
+		try {
+			date = LocalDate.of(year, month, day);
+			return true;
+		}catch(Exception e) {
+			System.out.println("Invalid input");
+			return false;
+		}
+	}
 	
 	
 }
