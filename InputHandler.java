a06d765adccc155e21ea6a3730d6c1fa35be6098
student8663 <110198971+student8663@users.noreply.github.com>
2023-12-09 13:17:09 -0800

    Update InputHandler.java

--- InputHandler.java
+++ InputHandler.java
@@ -1,7 +1,11 @@
 package CP;
 import java.time.*;
 import java.util.*;
+// month day year 를 int 로 받는 검사 + 제대로 된 날짜 검사 . flow chart 를 그려보면 approach 가 쉬울수 있따 
+// int 검사 3 개 while 과 hasnextint 이용해서 하면 된다 먼저 3 개 하고 날짜 검사이므로 do while 이용
+// int 도 getValidInt 만들어서 간략하게 줄일수 있다 
 
+//exception 만들고 출력하는거
 public class InputHandler {
 	public static Scanner scanner = new Scanner(System.in);
 	public static LocalDate date;
