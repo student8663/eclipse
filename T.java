434307509b1337467f99f03ca5b007497af5a783
student8663 <110198971+student8663@users.noreply.github.com>
2023-12-11 14:02:07 -0800

    Add files via upload

---
+++ T.java
@@ -0,0 +1,19 @@
+package SecondPackage;
+
+public class T {
+
+	public static void main(String[] args) {
+		StringBuilder stringB = new StringBuilder("Hello world");
+		stringB.append(" folks");
+		stringB.delete(0, 5);
+		stringB.insert(0, "Hello");
+	
+		System.out.println(stringB);
+		
+		String string = stringB.toString();	
+		System.out.println(string);
+	}
+	
+	
+
+}
